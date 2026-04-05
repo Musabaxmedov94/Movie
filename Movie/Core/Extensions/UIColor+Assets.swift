6a2bb2a49ab910f8f032c8b168a37ef8dfa23d81
Musabaxmedov <musabaxmedov@gmail.com>
2026-04-06 00:17:13 +0400

    feat: implement search functionality

--- Movie/Core/Extensions/UIColor+Assets.swift
+++ Movie/Core/Extensions/UIColor+Assets.swift
@@ -9,6 +9,18 @@ import UIKit
 
 extension UIColor {
     
+    convenience init(hex: String) {
+        let hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        var rgb: UInt64 = 0
+        Scanner(string: hexString).scanHexInt64(&rgb)
+        self.init(
+            red:   CGFloat((rgb >> 16) & 0xFF) / 255,
+            green: CGFloat((rgb >> 8) & 0xFF) / 255,
+            blue:  CGFloat(rgb & 0xFF) / 255,
+            alpha: 1.0
+        )
+    }
+    
     private static func asset(_ name: String, fallBack: UIColor) -> UIColor{
         guard let color = UIColor(named: name) else {
             assertionFailure("Missing color asset: \(name)")
--- Movie/Features/Search/SearchViewController.swift
+++ Movie/Features/Search/SearchViewController.swift
@@ -9,9 +9,63 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    private lazy var searchContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(hex: "3A3F47")
+        view.layer.borderColor = UIColor(hex: "67686D").cgColor
+        view.layer.borderWidth  = 0.5
+        view.layer.cornerRadius = 21
+        return view
+    }()
+    
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Search",
+            attributes: [ .foregroundColor : UIColor(hex: "67686D")]
+        )
+        textField.textColor = UIColor(hex: "FFFFFF")
+        return textField
+    }()
+    
+    private lazy var searchIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "magnifyingglass")
+        imageView.tintColor = UIColor(hex: "67686D")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .main
+        configureView()
         
     }
+    
+    private func configureView() {
+        view.backgroundColor = .main
+        view.addSubview(searchContainerView)
+        searchContainerView.addSubviews(searchTextField, searchIcon)
+        
+        searchContainerView
+            .top(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).0
+            .leading(equalTo: view.leadingAnchor, constant: 29).0
+            .trailing(equalTo: view.trailingAnchor, constant: -29).0
+            .height(constant: 42)
+        
+        searchIcon
+            .trailing(equalTo: searchContainerView.trailingAnchor, constant: -12).0
+            .centerY(equalTo: searchContainerView.centerYAnchor).0
+            .width(constant: 20).0
+            .height(constant: 20)
+        
+        searchTextField
+            .leading(equalTo: searchContainerView.leadingAnchor, constant: 12).0
+            .trailing(equalTo: searchIcon.leadingAnchor, constant: -8).0
+            .centerY(equalTo: searchContainerView.centerYAnchor)
+        
+    }
+    
+    
+    
 }
+
