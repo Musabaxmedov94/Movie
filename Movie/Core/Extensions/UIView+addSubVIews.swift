7a1894b91091e1c5cd38269551f6bd9c9dcbd955
Musabaxmedov <musabaxmedov@gmail.com>
2026-04-07 23:17:33 +0400

    refactor: replace UIView+Layout with UIView+addSubViews

---
+++ Movie/Core/Extensions/UIView+addSubVIews.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  BankApp
+//
+//  Created by musabaxmedov on 07.12.25.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+}
+
